df47e5cdc3a7c8e45c913cc44365063b57dd1d84
tanialemos <tanialemosribeiro@gmail.com>
2018-03-12 16:21:12 +0100

    StockService done

---
+++ api/src/main/java/com/tanialemos/api/StockExchangeController.java
@@ -0,0 +1,4 @@
+package com.tanialemos.api;
+
+public class StockExchangeController {
+}
--- service/src/main/java/com/tanialemos/service/StockService.java
+++ service/src/main/java/com/tanialemos/service/StockService.java
@@ -1,11 +1,24 @@
 package com.tanialemos.service;
 
+import com.tanialemos.domain.Stock;
+import com.tanialemos.domain.StockPrice;
+import com.tanialemos.domain.StockRepository;
 import com.tanialemos.interfaces.ExternalStockInformationService;
 import org.apache.commons.lang3.RandomUtils;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 public class StockService implements ExternalStockInformationService {
+
+    StockRepository stockRepository = new StockRepository();
+
+    public Stock getStock(String stockId){
+        Map<String, Stock> connectionToFakeDB = stockRepository.getFakeDB();
+        Stock stock = connectionToFakeDB.get(stockId);
+        stock.setPrice(new StockPrice(getPriceInEuroForStock(stockId)));
+        return stock;
+    }
 
     public BigDecimal getPriceInEuroForStock(String stockId) {
         double nbr = RandomUtils.nextDouble(10,100);
