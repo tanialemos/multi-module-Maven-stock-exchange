32845570b13e1de1859cb6ebd88271c1c8cfaf41
tanialemos <tanialemosribeiro@gmail.com>
2018-03-13 10:34:45 +0100

    StockServiceTest - getPriceInEuroForStock OK!

--- service/src/main/java/com/tanialemos/service/StockServiceTest.java
+++ service/src/main/java/com/tanialemos/service/StockServiceTest.java
@@ -3,22 +3,24 @@ package com.tanialemos.service;
 import com.tanialemos.domain.Stock;
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions;
+
 import java.math.BigDecimal;
 
 
 class StockServiceTest {
 
     StockService stockService = new StockService();
+    Stock stock = new Stock("test", "test_stock");
 
     @Test
     void getStock() {
+
     }
 
     @Test
     void getPriceInEuroForStock_givenAStockId_returnsRandomBigDecimalBetween10And100() {
-        Stock stock = new Stock("test", "test_stock");
         BigDecimal actualNbr = stockService.getPriceInEuroForStock("test");
 
-        Assertions.assertThat(actualNbr).isBetween(BigDecimal.valueOf(10),BigDecimal.valueOf(100));
+        Assertions.assertThat(actualNbr).isBetween(BigDecimal.valueOf(10), BigDecimal.valueOf(100));
     }
 }
